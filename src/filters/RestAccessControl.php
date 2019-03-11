8b5ecd7abb9a1e74bc8fcdd961d335980bc4b101
lijkbezorger <lijkbezorger@gmail.com>
2019-03-11 10:49:16 +0200

    Fix rest filter

--- src/filters/RestAccessControl.php
+++ src/filters/RestAccessControl.php
@@ -12,7 +12,6 @@ class RestAccessControl extends AccessControl
 {
     protected function setDenyCallBack()
     {
-        //TODO make json response
         /**
          * @param $rule AccessRule|null
          * @param $action ErrorAction|InlineAction
@@ -21,13 +20,15 @@ class RestAccessControl extends AccessControl
          */
         $this->denyCallback = function ($rule, $action) {
             $controller = $action->controller;
-            if ($this->userComponent->isGuest) {
-                return $controller->redirect($this->loginUrl);
-            }
-            Yii::$app->session->setFlash('warning', Yii::t('rbac', 'You don\'t have permission to')
-                . ' ' . Yii::t('rbac', 'do this action'));
 
-            return $controller->redirect(Yii::$app->request->referrer ?? Yii::$app->getHomeUrl());
+            $message = Yii::t('rbac', 'You don\'t have permission to')
+                . ' ' . Yii::t('rbac', 'do this action');
+
+            Yii::$app->response->setStatusCode(403);
+
+            return $controller->asJson([
+                'message' => $message,
+            ]);
         };
     }
 }
